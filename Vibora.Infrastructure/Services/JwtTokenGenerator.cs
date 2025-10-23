52f8cb885fb56e201dee5490c084eabd4b517bbd
Breno Trindade <breno.trindade2011@gmail.com>
2025-10-23 17:09:56 -0300

    feat(auth): Add expiration time to settings file

--- Vibora.Infrastructure/Services/JwtTokenGenerator.cs
+++ Vibora.Infrastructure/Services/JwtTokenGenerator.cs
@@ -24,20 +24,22 @@ public class JwtTokenGenerator : IJwtTokenGenerator
         var key = Encoding.ASCII.GetBytes(_configuration["Jwt:SecretKey"]);
 
         var claims = new List<Claim>
-        { 
+        {
             new Claim(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
-            new Claim(JwtRegisteredClaimNames.Sub, user.Name),
-            new Claim(JwtRegisteredClaimNames.Sub, user.Email)
+            new Claim(JwtRegisteredClaimNames.UniqueName, user.Name),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email)
         };
 
+        var expiryMinutes = _configuration.GetValue<int>("Jwt:ExpirationHours");
+
         var tokenDescriptor = new SecurityTokenDescriptor
-        { 
+        {
             Subject = new ClaimsIdentity(claims),
-            Expires = DateTime.UtcNow.AddHours(2),
+            Expires = DateTime.UtcNow.AddHours(expiryMinutes),
             Issuer = _configuration["Jwt:Issuer"],
             Audience = _configuration["Jwt:Audience"],
             SigningCredentials = new SigningCredentials(
-                new SymmetricSecurityKey(key),SecurityAlgorithms.HmacSha256Signature)
+                new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
 
         var token = tokenHandler.CreateToken(tokenDescriptor);
