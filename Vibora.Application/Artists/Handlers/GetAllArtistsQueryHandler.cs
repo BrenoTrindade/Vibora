b794e0492cc7aeba8badabd4a095bd37c2934c66
Breno Trindade <breno.trindade2011@gmail.com>
2025-11-06 22:16:47 -0300

    feat(artist): implement get all artists query

---
+++ Vibora.Application/Artists/Handlers/GetAllArtistsQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Vibora.Application.Artists.Common;
+using Vibora.Application.Artists.Queries;
+using Vibora.Domain.Repositories;
+
+namespace Vibora.Application.Artists.Handlers;
+
+public class GetAllArtistsQueryHandler : IRequestHandler<GetAllArtistsQuery, IEnumerable<ArtistResponse>>
+{
+    private readonly IArtistRepository _artistRepository;
+
+    public GetAllArtistsQueryHandler(IArtistRepository artistRepository)
+    {
+        _artistRepository = artistRepository;
+    }
+
+    public async Task<IEnumerable<ArtistResponse>> Handle(GetAllArtistsQuery request, CancellationToken cancellationToken)
+    {
+        var artists = await _artistRepository.GetAllAsync();
+
+        return artists.Select(artist => new ArtistResponse
+        {
+            Id = artist.Id,
+            Name = artist.Name,
+            Bio = artist.Bio,
+            ImageUrl = artist.ImageUrl,
+            AlbumIds = artist.Albums.Select(a => a.Id),
+            TrackIds = artist.Tracks.Select(t => t.Id)
+        });
+    }
+}
---
+++ Vibora.Application/Artists/Queries/GetAllArtistsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+using Vibora.Application.Artists.Common;
+
+namespace Vibora.Application.Artists.Queries;
+
+public class GetAllArtistsQuery : IRequest<IEnumerable<ArtistResponse>>
+{
+}
