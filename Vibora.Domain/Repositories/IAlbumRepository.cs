3d46fc154e2090ac0c1a12410512778780be6d46
Breno Trindade <breno.trindade2011@gmail.com>
2025-11-07 22:32:16 -0300

    feat(album): add album repository and interface

---
+++ Vibora.Domain/Repositories/IAlbumRepository.cs
@@ -0,0 +1,11 @@
+﻿using Vibora.Domain.Entities;
+
+namespace Vibora.Domain.Repositories;
+
+public interface IAlbumRepository
+{
+    Task Add(Album album);
+    Task<Album> GetById(Guid id);
+    Task<IEnumerable<Album>> GetAll();
+    Task Delete(Album album);
+}
---
+++ Vibora.Infrastructure/Repositories/AlbumRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vibora.Domain.Entities;
+using Vibora.Domain.Repositories;
+using Vibora.Infrastructure.Persistence;
+
+namespace Vibora.Infrastructure.Repositories;
+public class AlbumRepository : IAlbumRepository
+{
+    private readonly ViboraDbContext _dbContext;
+    public AlbumRepository(ViboraDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task Add(Album album)
+    {s
+        await _dbContext.Albums.AddAsync(album);
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task Delete(Album album)
+    {
+        _dbContext.Albums.Remove(album);    
+
+        await _dbContext.SaveChangesAsync();
+    }
+
+    public async Task<IEnumerable<Album>> GetAll()
+    {
+        return await _dbContext.Albums.AsNoTracking().ToListAsync();
+    }
+
+    public async Task<Album?> GetById(Guid id)
+    {
+        return _dbContext.Albums.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+    }
+}
